ca3a691791a5b007922b394e4ed0c644b8bc0a1b
JereLew <jeremiahhclew@gmail.com>
2023-03-16 21:26:49 -0400

    Commit message for midterm.

---
+++ lsp-spring-2023/src/org/howard/edu/hw4/Listemptyexception.java
@@ -0,0 +1,12 @@
+package org.howard.edu.hw4;
+
+public class Listemptyexception extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lsp-spring-2023/src/org/howard/edu/lsp/midterm/problem51/EmptyRangeException.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.problem51;
+
+public class EmptyRangeException extends Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
