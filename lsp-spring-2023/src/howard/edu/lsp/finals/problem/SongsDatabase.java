78210f161cd97d28491522ec77edb7fb1acb6e34
JereLew <jeremiahhclew@gmail.com>
2023-04-27 20:26:34 -0400

    Commit for final exam.

---
+++ lsp-spring-2023/src/howard/edu/lsp/finals/problem/SongsDatabase.java
@@ -0,0 +1,10 @@
+package howard.edu.lsp.finals.problem;
+
+public class SongsDatabase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lsp-spring-2023/src/howard/edu/lsp/finals/problem/SongsDatabaseTest.java
@@ -0,0 +1,10 @@
+package howard.edu.lsp.finals.problem;
+
+public class SongsDatabaseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
